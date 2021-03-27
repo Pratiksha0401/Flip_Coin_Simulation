6f362b31f63d6f904c52aae1aece95e0faa54688
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-27 20:08:32 +0530

    Added : code to show number of times head and tail has wons

--- flip_Coin_Simulator.sh
+++ flip_Coin_Simulator.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -x
 H=1
 T=0
-x=$((RANDOM%2))
-if [ $x -eq $H ]
-then
-        echo "Head wins"
-else
-        echo "Tail wins"
-fi
+countH=0
+countT=0
+read -p "Enter number for which you want to flip a coin : " num
+for (( i=1;i<=$num;i++ ))
+do
+	x=$((RANDOM%2))
+	if [ $x -eq $H ]
+	then
+		((countH++))
+	else
+		((countT++))
+	fi
+done
+echo "Head wins $countH times"
+echo "Tail wins $countT times"
