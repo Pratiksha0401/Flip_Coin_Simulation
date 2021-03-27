fe91b2e1a56e3d312b83eda34d0d7088368994cc
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-27 20:38:18 +0530

    Added : code of who wins 21 times first and by how much

--- flip_Coin_Simulator.sh
+++ flip_Coin_Simulator.sh
@@ -3,16 +3,33 @@ H=1
 T=0
 countH=0
 countT=0
-read -p "Enter number for which you want to flip a coin : " num
-for (( i=1;i<=$num;i++ ))
+for (( i=1;i<=42;i++ ))
 do
 	x=$((RANDOM%2))
 	if [ $x -eq $H ]
 	then
+		if [ $countH -eq 21 ]
+		then
+			break
+		fi
 		((countH++))
 	else
+		if [ $countT -eq 21 ]
+		then
+			break
+		fi
 		((countT++))
 	fi
 done
-echo "Head wins $countH times"
-echo "Tail wins $countT times"
+
+if [ $countH -eq $countT ]
+then
+	echo "Tie between Head and Tail i.e 21 all"
+elif [ $countH -gt $countT ]
+then
+	result1=$(($countH-$countT))
+	echo "Head wins $countH times and won by $result1"
+else
+	result2=$(($countT-$countH))
+	echo "Tail wins $countT times and won by $result2"
+fi
