6ede23f060499acb51c39114d1cd2f9655c5af78
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-28 16:30:03 +0530

    Added : code if its tie then contiues till the difference of 2 pt achieved

--- flip_Coin_Simulator.sh
+++ flip_Coin_Simulator.sh
@@ -25,11 +25,33 @@ done
 if [ $countH -eq $countT ]
 then
 	echo "Tie between Head and Tail i.e 21 all"
+	for (( j=1;j<=11;j++ ))
+	do
+		x=$((RANDOM%2))
+		if [ $x -eq $H ]
+		then
+			((countH++))
+			result1=$(($countH-$countT))
+			if [ $result1 -eq 2 ]
+      	then
+				echo "and Head won by $result1 points"
+         	break
+			fi
+	 	else
+			((countT++))
+			result2=$(($countT-$countH))
+      	if [ $result2 -eq 2 ]
+      	then
+				echo "and Tail won by $result2 points"
+         	break
+      	fi
+   	fi
+	done
 elif [ $countH -gt $countT ]
 then
-	result1=$(($countH-$countT))
-	echo "Head wins $countH times and won by $result1"
+	result3=$(($countH-$countT))
+	echo "Head wins $countH times and won by $result3 points"
 else
-	result2=$(($countT-$countH))
-	echo "Tail wins $countT times and won by $result2"
+	result4=$(($countT-$countH))
+	echo "Tail wins $countT times and won by $result4 points"
 fi
